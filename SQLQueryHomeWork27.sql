9a6d03cd861fb02e05b9a34e2a3cf203d9e191cd
Paul-cmd29 <paulpetrov8510@gmail.com>
2022-10-03 11:05:40 +0300

    initial commit

---
+++ SQLQueryHomeWork27.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE Persons
+
+CREATE TABLE PersonsNew
+(
+	Id INT IDENTITY,
+	FirstName NVARCHAR(20) NOT NULL,
+	SecondName NVARCHAR(20) NOT NULL,
+	Age INT,
+	Adress NVARCHAR(30),
+	PhoneNumber VARCHAR(20) UNIQUE,
+	Email VARCHAR(20) UNIQUE,
+)
+
+ALTER TABLE PersonsNew 
+ADD Gender NVARCHAR(200);
+SELECT * FROM PersonsNew
+WHERE Gender = Male
+
+SELECT * FROM  PersonsNew 
+WHERE Age > 18
+
+SELECT * FROM  PersonsNew 
+WHERE Adress IS NULL
+
+UPDATE PersonsNew
+SET Age += 1 
+
+DELETE FROM PersonsNew
+	WHERE Adress IS NULL
+SELECT * FROM  PersonsNew 
+
+SELECT COUNT(*) FROM PersonsNew
+
+SELECT Age, COUNT(*) As AgeGroupBy
+FROM PersonsNew
+GROUP BY Age
